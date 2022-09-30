ef013c538bcdb4bc3008adfe3d338124e4e9600a
dayo2n <0217dayun@naver.com>
2022-09-30 22:58:27 +0900

    [AES] AES-128-ECB 구현

--- CipherCommunicator/client.py
+++ CipherCommunicator/client.py
@@ -17,16 +17,19 @@ class Receiver(Thread):
         super().__init__()
         self.socket = socket
 
-    def decrypt(self, ciphertext:bytes) -> bytes:
+    def decrypt(self, ciphertext: bytes) -> bytes:
         # place your own implementation of
         # AES-128-ECB decryption with pycryptodome
 
-        return b''
+        # AES-128-ECB 객체 생성 후 복호화
+        decrypted_txt = AES.new(ENCRYPTION_KEY, AES.MODE_ECB).decrypt(ciphertext)
+        unpad_decrypted_txt = unpad(decrypted_txt, BLOCK_SIZE)  # unpadding
+        return unpad_decrypted_txt
 
     def handle_recv(self, received:bytes):
         try:
             decrypt_result = self.decrypt(received)
-            print("Received: " + bytes.decode(decrypt_result, "UTF-8"))
+            print("\nReceived: " + bytes.decode(decrypt_result, "UTF-8"))
         except:
             pass
 
@@ -35,11 +38,15 @@ class Receiver(Thread):
             received:bytes = self.socket.recv(1024)
             self.handle_recv(received)
 
-def encrypt_message(msg: bytes) -> bytes:
+def encrypt_message(msg_encoded: bytes) -> bytes:
     # place your own implementation of
     # AES-128-ECB encryption with pycryptodome
 
-    return b''
+    padded_txt = pad(msg_encoded, BLOCK_SIZE)  # padding
+    # AES 객체 생성 후 암호화
+    encrypted_txt = AES.new(ENCRYPTION_KEY, AES.MODE_ECB).encrypt(padded_txt)
+
+    return encrypted_txt
 
 client_socket = socket(AddressFamily.AF_INET, SocketKind.SOCK_STREAM)
 client_socket.connect(('127.0.0.1', 24000))
@@ -56,9 +63,9 @@ Receiver(client_socket).start()
 
 while True:
     msg = input("Message: ")
-    msg_encoded = msg.encode("UTF-8")
+    msg_encoded = msg.encode("UTF-8")  # bytes로 변환
 
     payload = encrypt_message(msg_encoded)
     client_socket.send(payload)
-    
+
     print("Me: " + msg)
